165af58e61cae6818ff9d7494be5d54326186be8
przemyslvw <przemyslvw@gmail.com>
2019-07-30 20:20:04 +0200

    router-view animation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="my-app">
     <b-container>
-      <router-view></router-view>
+      <transition name="fade" mode="out-in">
+        <router-view></router-view>
+      </transition>
     </b-container>
   </div>
 </template>
@@ -23,5 +25,15 @@ export default {
 <style lang="scss">
 .my-app {
   padding: 50px 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.8s;
+}
+
+.fade-enter,
+.fade-leave-toe {
+  opacity: 0;
 }
 </style>
